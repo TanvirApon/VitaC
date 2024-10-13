d0d7348c19079aa72c42d98f0c629d08d54e1638
TanvirApon <tanvirhossainapon@gmail.com>
2024-10-13 13:02:29 +0600

    Code Updated

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Js File has been added");
+
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("read-btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
